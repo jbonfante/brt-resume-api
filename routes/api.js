4ea81bb4d46d2f348488c5e2a5082b4dd9862658
Juan Bonfante <juan.bonfante@gmail.com>
2017-07-20 15:13:36 -0500

    Added request Params hash/map object for incoming query params, and the responses.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var requestMap = {"Ping":"OK",
+  "Name":"Juan Bonfante",
+  "Years":"+12",
+  "Referrer":"Addison Search",
+  "Position":"Software Engineer",
+  "Status":"US Citizen",
+  "Phone":"773-679-4909",
+  "Resume":"http://bit.ly/2gO9Y9j",
+  "Source":"https://github.com/jbonfante/brt-resume-api",
+  "Degree":"None",
+  "Puzzle":"Not Sure yet",
+  "Email+Address":"juan.bonfante@gmail.com"}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('OK');
+  console.log(req.query);
+  var responseText = requestMap[req.query.q] || "nothing";
+  res.send(responseText);
 });
 
 module.exports = router;
